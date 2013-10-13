3c2bb40b13e8bdd7d08485faea18c66960b9f2ed
Vivek Seth <Vivekseth.m@gmail.com>
2013-10-13 02:57:14 -0400

    dynamically change socket urls based on current url, to fix port issue

--- assets/js/host.js
+++ assets/js/host.js
@@ -1,7 +1,15 @@
+//added this block to get aroung port issue
+//--- hacky.... but works
+var curr_url = document.URL;
+var new_url = curr_url.replace("3000", "5000");
+var s = document.createElement( 'script' );
+s.setAttribute( 'src', new_url+"socket.io/socket.io.js");
+document.write(s.outerHTML);
+//---
 var socket = null;
 window.onload = function() {
 	if (io) {
-		socket = io.connect('http://localhost');
+		socket = io.connect(new_url);
 		socket.on("message", function (data) {
 			console.log(data["message"]);
 		});
@@ -13,4 +21,9 @@ window.onload = function() {
 }
 function send(message) {
 	socket.emit("message", {"message" : message});
+}
+function addScript( src ) {
+  var s = document.createElement( 'script' );
+  s.setAttribute( 'src', src );
+  document.body.appendChild( s );
 }
