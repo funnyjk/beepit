4b5967a6c0c00b5fc89a0d59d2be3de4c449dae8
Vivek Seth <Vivekseth.m@gmail.com>
2013-10-12 21:16:41 -0400

    fixed error in host.js

--- assets/js/host.js
+++ assets/js/host.js
@@ -1,9 +1,10 @@
-window.onLoad = function() {
-	var socket = io.connect('http://localhost:5000');
-	var send = function(message) {
-		socket.emit("message", {"message" : message});
-	}
-	socket.on("message", function (data) {
-		console.log(data["message"]);
-	});
+var socket = null;
+window.onload = function() {
+	socket = io.connect('http://localhost:5000');
+}
+socket.on("message", function (data) {
+	console.log(data["message"]);
+});
+function send(message) {
+	socket.emit("message", {"message" : message});
 }
