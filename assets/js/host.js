67d53526fe8a336252d0db6cd5c3251e752c2f9c
funnyjk <funnyjk@gmail.com>
2013-10-12 21:04:33 -0400

    index looks for host

--- assets/js/host.js
+++ assets/js/host.js
@@ -1,6 +1,6 @@
 window.onLoad = function() {
 
-	var socket = io.connect("http://localhost");
+	var socket = io.connect("http://localhost:5000");
 	var action = document.getElementById("action");
 	var test = [];
 	
