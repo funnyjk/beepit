ec311b37caa718068c2d5e50025dcbd993ed7877
Vivek Seth <Vivekseth.m@gmail.com>
2013-10-12 20:32:03 -0400

    added some code for socket.io, using client side "send" function, the servers should recieve and emit the an altererd version of the message.

---
+++ app.js
@@ -0,0 +1,23 @@
+var 
+	express = require('express'),
+	app = express(),
+	server = require('http').createServer(app), 
+	io = require('socket.io').listen(server)
+;
+
+app.configure(function(){
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.on('message', function (data) {
+  	data["message"] = "SERVER: " + data["message"];
+  	socket.emit('message', data);
+  });
+});
+
+app.listen(3000);
