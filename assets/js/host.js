3263dcae99fc78378f35540632587419e7520530
Vivek Seth <Vivekseth.m@gmail.com>
2013-10-13 03:43:02 -0400

    fixed small error

--- assets/js/host.js
+++ assets/js/host.js
@@ -1,13 +1,7 @@
-//added this block to get aroung port issue
-//--- hacky.... but works
-// var curr_url = document.URL;
-// var new_url = curr_url.replace("3000", "5000");
-// var s = document.createElement( 'script' );
-// s.setAttribute( 'src', new_url+"socket.io/socket.io.js");
-// document.write(s.outerHTML);
-//---
 var socket = null;
 window.onload = function() {
+	var curr_url = document.URL;
+	var new_url = curr_url.replace("3000", "5000");
 	if (io) {
 		socket = io.connect(new_url);
 		socket.on("message", function (data) {
