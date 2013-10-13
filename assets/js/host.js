55720a46491475d5c1ad77ea5df8e24ba90696de
Vivek Seth <Vivekseth.m@gmail.com>
2013-10-13 03:41:17 -0400

    now using static socket js file

--- assets/js/host.js
+++ assets/js/host.js
@@ -1,10 +1,10 @@
 //added this block to get aroung port issue
 //--- hacky.... but works
-var curr_url = document.URL;
-var new_url = curr_url.replace("3000", "5000");
-var s = document.createElement( 'script' );
-s.setAttribute( 'src', new_url+"socket.io/socket.io.js");
-document.write(s.outerHTML);
+// var curr_url = document.URL;
+// var new_url = curr_url.replace("3000", "5000");
+// var s = document.createElement( 'script' );
+// s.setAttribute( 'src', new_url+"socket.io/socket.io.js");
+// document.write(s.outerHTML);
 //---
 var socket = null;
 window.onload = function() {
