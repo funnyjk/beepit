9d1133f241fe75852b5b7b27394a1ad6df9f0960
Vivek Seth <Vivekseth.m@gmail.com>
2013-10-12 21:10:24 -0400

    moved simple socket code to host.js

--- assets/js/host.js
+++ assets/js/host.js
@@ -1,8 +1,9 @@
 window.onLoad = function() {
-
-	var socket = io.connect("http://localhost:5000");
-	var action = document.getElementById("action");
-	var test = [];
-	
-	socket.on('action', function(data))
-
+	var socket = io.connect('http://localhost:5000');
+	var send = function(message) {
+		socket.emit("message", {"message" : message});
+	}
+	socket.on("message", function (data) {
+		console.log(data["message"]);
+	});
+}
