0a69c1ff927272b183f973dc346e1c448a9aa1b8
funnyjk <funnyjk@funnyjk-1000HE.(none)>
2013-10-20 18:22:35 -0400

    adding mongodb and mongoosejs to game logic and code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,31 +1,27 @@
 var NUM_ACTIONS = 3;
-var INITIAL_WAIT_TIME = 3000;
-var DECREMENT_WAIT_TIME = 100;
-var MINIMUM_WAIT_TIME = 500;
 
 function randAction() {
-	var action = Math.floor(Math.random() * NUM_ACTIONS) + 1;
-	return action;
+        var action = Math.floor(Math.random() * NUM_ACTIONS) + 1;
+        return action;
 }
+
 
 //Game Loop
 var gameLoop = function() {
-	var action = randAction();
-	console.log(action);
+        var action = randAction();
+        console.log(action);
 }
 
 var intervalLoop = function() {
-	gameLoop();
-	console.log(waitTime);
-	if(waitTime > MINIMUM_WAIT_TIME) {
-		waitTime -= DECREMENT_WAIT_TIME;
-	}
+        gameLoop();
+        console.log(waitTime);
+        if(waitTime > 500) {
+                waitTime -= 500;
+        }
 
-	setTimeout(intervalLoop, waitTime);
+        setTimeout(intervalLoop, waitTime);
 }
 
 //Timing Logic
-var waitTime = INITIAL_WAIT_TIME;
+var waitTime = 3000;
 setTimeout(intervalLoop, waitTime);
-
-	
---
+++ web.js
@@ -0,0 +1,13 @@
+var	mongoose = require('mongoose'),
+	db = mongoose.Schema;
+
+var	bopSchema  = new db ({
+		gameID: Number,
+		actnum: Number,
+		time: Number,
+		timeInt: Number,
+		action: String
+	});
+
+	mongoose.model('game', bopSchema);
+	mongoose.connect('localhost', 'beepit';
